a2412dadfbb177ffec4d6807b191e49f75b429e9
cnsaunders <claire@Claires-MacBook-Pro.local>
2015-02-16 16:50:55 -0500

    Classes for Patient
    Pushing Date, Diagnosis, Medicine, and Name classes

---
+++ app/src/main/java/medicalfaxnew/duqsp15/com/medicalfax/Model/Patient/Diagnosis.java
@@ -0,0 +1,40 @@
+public class Diagnosis {
+
+	String primaryDiagnosis;
+	String secondaryDiagnosis;
+	String complications;
+
+	public Diagnosis(String primaryDiagnosis) {
+		primaryDiagnosis = this.primaryDiagnosis;
+	}
+
+	public Diagnosis(String primaryDiagnosis, String secondaryDiagnosis) {
+		primaryDiagnosis = this.primaryDiagnosis;
+		secondaryDiagnosis = this.secondaryDiagnosis;
+	}
+
+	public void setPrimaryDiagnosis(String newPrimaryDiagnosis) {
+		primaryDiagnosis = newPrimaryDiagnosis;
+	}
+
+	public void setSecondaryDiagnosis(String newSecondaryDiagnosis) {
+		secondaryDiagnosis = newSecondaryDiagnosis;
+	}
+	
+	public void setComplications(String newComplications) {
+		complications = newComplications;
+	}
+	
+	public String getPrimaryDiagnosis() {
+		return primaryDiagnosis;
+	}
+	
+	public String getSecondaryDiagnosis() {
+		return secondaryDiagnosis;
+	}
+	
+	public String getComplications() {
+		return complications;
+	}
+	
+}
