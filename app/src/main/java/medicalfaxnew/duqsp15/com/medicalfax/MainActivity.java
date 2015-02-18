f24f33839d18b611d317183f1e9b779c245fceff
Brady Sheehan <Bradya.sheehan@gmail.com>
2015-02-18 18:43:57 -0500

    Dictation Stuff
    Edited Dictation file, xml files for button click stuff related to
    demoing, and the main method with calls to Dictation

--- app/src/main/java/medicalfaxnew/duqsp15/com/medicalfax/MainActivity.java
+++ app/src/main/java/medicalfaxnew/duqsp15/com/medicalfax/MainActivity.java
@@ -1,4 +1,5 @@
 package medicalfaxnew.duqsp15.com.medicalfax;
+import medicalfaxnew.duqsp15.com.medicalfax.Model.Dictation.Dictation;
 
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
@@ -7,15 +8,45 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.Locale;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.speech.RecognizerIntent;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button dictateButton;
+    private EditText fullNameDisplay; //no matter what you do, its going to be displayed here
+    public Dictation dictation;
+    private final int REQ_CODE_SPEECH_INPUT = 100; //magic constant I don't understand yet
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        /*the code below was added by Brady Sheehan to demonstrate speech to text functionality
+        *
+        * 2/18/2015
+        * */
+        dictation = new Dictation(this);//we must pass the activity to use it outside the class
+
+        dictateButton = (Button) findViewById(R.id.Dictate_Button); //allows us to listen for click
+        fullNameDisplay = (EditText) findViewById(R.id.Full_Name); //how we can display it in this text view
+
+        dictateButton.setOnClickListener(new View.OnClickListener(){ //temporary listening for button click
+            @Override
+            public void onClick(View V){
+                dictation.getSpeechInput(); //calls the method in getSpeechInput
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -38,6 +69,22 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /*this method was written by Brady Sheehan on 2/18/2015
+    * it is meant to handle the resultant activity of getting speech input
+    *
+    * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        //speech recognition (like this) must require an internet connection!
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQ_CODE_SPEECH_INPUT && resultCode == RESULT_OK && null != data) {
+            ArrayList<String> result = dictation.returnSpeech(data); //returnSpeech(data) will return an arraylist<STRING>
+            fullNameDisplay.setText(result.get(0)); //hard coded to just display in one field - fullNameDisplay
+        }
+    }
+
     /** This method is called when Up Button is clicked
      *  It causes the cursor to move up once to the text field above the current position
      *  Created by: Kinardi Isnata, February 16 2015
--- app/src/main/java/medicalfaxnew/duqsp15/com/medicalfax/Model/Dictation/Dictation.java
+++ app/src/main/java/medicalfaxnew/duqsp15/com/medicalfax/Model/Dictation/Dictation.java
@@ -6,27 +6,28 @@ import java.util.Locale;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.os.Bundle;
 import android.speech.RecognizerIntent;
-import android.view.Menu;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import medicalfaxnew.duqsp15.com.medicalfax.R;
 
 /**
  * Created by austinpilz on 2/16/15.
  * Edited by Brady Sheehan and Justin Chilleo on 2/16/15
+ * Added getSpeechInput() and returnSpeech() methods - Brady Sheehan 2/18/15
  */
 public class Dictation
 {
+    public Activity activity;
     private boolean processing;
+    private final int REQ_CODE_SPEECH_INPUT = 100; //magic constant I don't understand yet
 
-    public Dictation()
+    public Dictation(Activity ac) //could pass the value of the activity to this constructor and then
     {
-        //
+        activity = ac;
     }
     
+/*
     //method for signifying start of dictation
     //that way they can signify a loading bar or something (??)
     public boolean dictationStarted(){
@@ -35,13 +36,38 @@ public class Dictation
         getSpeech();
         return true;
 
-    }
+    }*/
     
+/*
     //method that will actually return the speechs
     public String getSpeech(){
         String s = "Sorry, dictation is currently down";
         return s;
         
     }
-    
+*/
+
+    //helps in displaying speech input
+    public void getSpeechInput(){
+
+        //this creates its own activity
+        //http://developer.android.com/reference/android/app/Activity.html#startActivityForResult%28android.content.Intent%2c%20int%29
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        try{
+            activity.startActivityForResult(intent, REQ_CODE_SPEECH_INPUT); //this is called on the implicit activity that was created
+            //can't have intents without an activity
+        }catch(ActivityNotFoundException a){
+            Toast.makeText(activity.getApplicationContext(),activity.getString(R.string.speech_not_supported),Toast.LENGTH_SHORT ).show();
+        }
+    }
+
+    //sort of unnecessary.. added to show that this is the only way
+    //i found for the calling functions back and forth to work
+    public ArrayList<String> returnSpeech(Intent data){
+        ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+        return result;
+    }
 }
